5dee6c8530f38b78a743bc623f9edae62a5f44cf
IrisButigan <ibutigan@pmfst.hr>
2023-05-18 23:12:14 +0200

    v7

---
+++ Vjezbe/Vjezbe_7/program.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import projectile as pro
+
+dt = [0.1, 0.01, 0.001]
+
+for dt_i in dt:
+    pr = pro.Projectile(0.1, 20, 60, dt_i)
+    pr.gibanje_euler()
+    plt.plot(pr.x, pr.y, label='dt = {}'.format(dt_i))
+
+pr_rk4 = pro.Projectile(0.1, 20, 60, 0.01)
+pr_rk4.gibanje_rk4()
+plt.plot(pr_rk4.x, pr_rk4.y, label='dt = 0.01 RK4')
+
+plt.title('x-y')
+plt.xlabel('x / m')
+plt.ylabel('y / m') 
+plt.legend()
+plt.show()
